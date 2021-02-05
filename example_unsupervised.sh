bc82dc92343e0cea4f8e89e04c551ee4f3d42c05
Colby Ham <colby.ham@pnnl.gov>
2021-02-05 11:02:18 -0800

    Update example_unsupervised to work with our dataset

--- example_unsupervised.sh
+++ example_unsupervised.sh
@@ -1,1 +1,1 @@
-python -m graphsage.unsupervised_train --train_prefix ./example_data/ppi --model graphsage_mean --max_total_steps 1000 --validate_iter 10
+python -m graphsage.unsupervised_train --train_prefix ./example_data/last-fm --model graphsage_mean --max_total_steps 1000 --validate_iter 10 --base_log_dir results/ 
